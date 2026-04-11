1b3b5a568a6b2d5dcf63f5a161b20ff52e819802
abhaysingh-22 <abhaysingh6575@gmail.com>
2026-04-12 00:19:32 +0530

    added clipboard icon

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -482,3 +482,23 @@ export const LinkArrow = ({ className, ...rest }) => (
   </svg>
 );
 
+export const ClipboardIcon = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...rest}
+    className={`w-full h-auto ${className}`}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2m-1-2h8a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1Z"
+    />
+  </svg>
+);
+
