d33e34c6f75b4b3d42d95456d6143c6852d9432e
abhaysingh-22 <abhaysingh6575@gmail.com>
2026-04-12 00:21:56 +0530

    resume updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs '> As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in Devops, cloud and MERN.</p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="/sid.pdf" target={"_blank"}
+                <Link href="/abhayy.pdf" target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                   hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                   
