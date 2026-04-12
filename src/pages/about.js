bb0b38e83d6cbe6e14506ac52de6e5d6ab7a25f1
abhaysingh-22 <abhaysingh6575@gmail.com>
2026-04-12 12:13:26 +0530

    fixed image

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
-import profilePic from '../../public/images/profile/sid5.png'
+import profilePic from '../../public/images/profile/abe.jpg'
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
@@ -75,7 +75,7 @@ const about = () => {
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light
                               
                               ' />
-                            <Image src={profilePic} alt='sidharth' className='w-full h-auto rounded-2xl'
+                            <Image src={profilePic} alt='abe' className='w-full h-auto rounded-2xl'
                                 priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                         </div>
                         <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center
