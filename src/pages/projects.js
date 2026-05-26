1ac58214e48e419a3ff157c88f9928412c38d4e6
abhaysingh-22 <abhaysingh6575@gmail.com>
2026-05-26 22:10:13 +0530

    fixed the project tab

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -178,7 +178,7 @@ const projects = () => {
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
-            text="Imagination Trumps Knowledge"
+            text="Projects & Experiences"
             className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl  xs:!text-4xl"
           />
 
